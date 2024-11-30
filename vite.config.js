e9d1dfc063ec51ff39fd22ea2db601123ce9caa4
Salman Qureshi <salman.iqureshi04@gmail.com>
2024-11-30 23:08:57 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_PUBLIC_API,
+        target:"http://localhost:8000" ,
         changeOrigin: true,
       },
     },
